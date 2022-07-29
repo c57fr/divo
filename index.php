1e59ea90397e20ac2665f7fda27b26c798e3eba9
Crea7Net <l.cote@codeur.online>
2022-07-29 13:05:52 +0200

    Up Title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>MySQL</title>
   <script>
   //<![CDATA[
   document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.10'><\/script>"
