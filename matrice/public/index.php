fc5c3babc118afb4c66b3f131bd78232f6c5031e
Lionel COTE <grcote7@gmail.com>
2022-08-24 11:30:12 +0200

    Base bootstrap

--- matrice/public/index.php
+++ matrice/public/index.php
@@ -1,37 +1,26 @@
 <?php
 
-/**
- * (ɔ) Online FORMAPRO - GrCOTE7 -2022.
- */
-
-// On charge l'autoload qui contient les packages de composer
 include_once '../functions/functions.php';
+include_once '../functions/functions_menu.php';
+// On charge l'autoload qui contient les packages de composer
 require_once '../vendor/autoload.php';
-// $loader = new \Twig\Loader\ArrayLoader([
-// 	'index' => "Hello, {{ name }}<br><br>
-//   {{ ['Rémi', 'Pierre', 'Paul', 'Jacques'] | join(', ',' & ') }} !",
-// ]);
-// $twig = new \Twig\Environment($loader);
-// echo $twig->render('index', ['name' => 'Lionel']);
 
 // On charge le loader Twig et on lui indique où se trouvent les templates HTML
-$loader = new \Twig\Loader\FilesystemLoader('./views/pages/');
+$loader = new \Twig\Loader\FilesystemLoader('views');
 // On desactive le cache
 $twig = new \Twig\Environment($loader, [
     'cache' => false,
     'debug' => true,
     ]);
 
-
 // On charge la vue Twig
-$template = $twig->load('./views/pages/home1.htm');
-die('123456');
+$template = $twig->load('./pages/home.twig');
 
 $title = 'Home';
 
 // On rend notre vue en lui passant des variables si besoin
 echo $template->render(
     [
-        'titre' => $title,
+        'title' => $title,
     ]
 );
