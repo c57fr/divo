a060637a5558bc546dfed3665d1429986e00439b
Lionel COTE <grcote7@gmail.com>
2022-08-25 08:37:17 +0200

    Add CS Fixer

--- matrice/functions/functions_menu.php
+++ matrice/functions/functions_menu.php
@@ -1,17 +1,18 @@
 <?php
 
 /**
- * (ɔ) Online FORMAPRO - GrCOTE7 - 2022.
+ * (ɔ) Online FORMAPRO - GrCOTE7 -2022.
  */
 
 function nav_item(string $file, string $title, string $linkClass = '')
 {
-    $classe = 'nav-link';
-    if ($_SERVER['SCRIPT_NAME'] === '/php/tests/php/ga/html/' . $file) {
-        $classe .= ' active';
-    }
+	$classe = 'nav-link';
+	aff($_SERVER['SCRIPT_NAME'], 'SN');
+	if ($_SERVER['SCRIPT_NAME'] === '/matrice/public/' . $file) {
+		$classe .= ' active';
+	}
 
-    return <<<HTML
+	return <<<HTML
     <li class="{$linkClass}">
       <a class="{$classe}" aria-current="page" href="./{$file}">{$title}</a>
     </li>
@@ -20,5 +21,10 @@ function nav_item(string $file, string $title, string $linkClass = '')
 
 function nav_menu(string $linkClass = ''): string
 {
-    return nav_item('index.php', 'Accueil', $linkClass) . nav_item('jeu.php', 'Jeu', $linkClass) . nav_item('contact.php', 'Contact', $linkClass);
+	return nav_item('index.php', 'Accueil', $linkClass) . nav_item('jeu.php', 'Jeu', $linkClass) . nav_item('contact.php', 'Contact', $linkClass);
 }
+
+$nav_item = nav_menu('nav_item');
+$nav_menu = nav_menu('nav_item');
+aff($nav_item);
+aff($nav_menu);
